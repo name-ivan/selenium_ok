b79ff8a4a3f68f2b7fa1d6877fbce9cd9f1ffa34
Ivan <ivan.sukhin26@gmail.com>
2025-05-31 09:46:57 +0800

    2nd part started

--- lesson_2/test_lesson2.py
+++ lesson_2/test_lesson2.py
@@ -140,7 +140,19 @@ def test_same_cards(driver):
         print(card.find_element(By.CLASS_NAME, 'price').text)
     # print(product_cards[0].find_element(By.CLASS_NAME, 'price').text)
     
-    
+def test_tab(driver):
+    driver.get('https://www.qa-practice.com/elements/new_tab/link')
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'new-page-link')))
+    driver.find_element(By.ID, 'new-page-link').click()
+    tabs = driver.window_handles
+    driver.switch_to.window(tabs[1])
+    # WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'result-text')))
+    # driver.get('https://www.qa-practice.com/elements/new_tab/new_page')
+    result_text = driver.find_element(By.CSS_SELECTOR, '.result-text')
+    assert result_text.text == 'I am a new page in a new tab'
+    driver.close()
+    driver.switch_to.window(tabs[0])
+
     
     
     
